84efccd5451ee0b411d260493c3fa8ef5977c26d
williams manrique <wmanriques@gmail.com>
2012-10-02 18:19:20 -0500

    init

---
+++ wmanriques/src/pe/proyecto/EditorManaged.java
@@ -0,0 +1,27 @@
+package pe.proyecto;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EditorManaged {
+	private String value = "williams";
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	public void save(ActionEvent actionEvent) {  
+        FacesContext context = FacesContext.getCurrentInstance();  
+          
+        context.addMessage(null, new FacesMessage("Successful", "Hello " + value));  
+        context.addMessage(null, new FacesMessage("Second Message", "Additional Info Here..."));  
+    } 
+}
