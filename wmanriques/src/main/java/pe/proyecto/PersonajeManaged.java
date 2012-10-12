b1f3ec824f65558f2939eabbf53bdf20c5efbd99
williams manrique <wmanriques@gmail.com>
2012-10-12 00:33:57 -0500

    not warnings

--- wmanriques/src/main/java/pe/proyecto/PersonajeManaged.java
+++ wmanriques/src/main/java/pe/proyecto/PersonajeManaged.java
@@ -9,7 +9,6 @@ import javax.faces.event.ActionEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 
 @Controller
 public class PersonajeManaged {
