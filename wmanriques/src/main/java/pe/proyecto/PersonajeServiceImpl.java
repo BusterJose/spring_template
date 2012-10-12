6f20a22579aa17d461f2a9355260db65e5dbac8a
williams manrique <wmanriques@gmail.com>
2012-10-12 01:25:07 -0500

    log4j ok

--- wmanriques/src/main/java/pe/proyecto/PersonajeServiceImpl.java
+++ wmanriques/src/main/java/pe/proyecto/PersonajeServiceImpl.java
@@ -2,6 +2,8 @@ package pe.proyecto;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -12,11 +14,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class PersonajeServiceImpl implements PersonajeService {
 
+	private Log log = LogFactory.getLog(PersonajeServiceImpl.class);
+	
 	@Autowired
 	private PersonajeMapperXML personajeMapperXML;
 
 	@Transactional(readOnly = true)
 	public List<Personaje> findAll() {
+		log.info("fillAll() ok");
 		List<Personaje> personajes = personajeMapperXML.findAll();
 		return personajes;
 	}
