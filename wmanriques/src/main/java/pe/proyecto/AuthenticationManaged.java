61812f0d882bf4ffe1c1a0e3440f0128792f4fc6
williams manrique <wmanriques@gmail.com>
2012-10-20 08:28:31 -0500

    template security 70%

---
+++ wmanriques/src/main/java/pe/proyecto/AuthenticationManaged.java
@@ -0,0 +1,67 @@
+package pe.proyecto;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AuthenticationManaged {
+
+	private String user = null;
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public AuthenticationManager getAuthenticationManager() {
+		return authenticationManager;
+	}
+
+	public void setAuthenticationManager(
+			AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+	}
+
+	private String password = null;
+
+	@Autowired
+	private AuthenticationManager authenticationManager = null;
+
+	public String login() {
+
+		try {
+
+			Authentication request = new UsernamePasswordAuthenticationToken(
+					this.getUser(), this.getPassword());
+
+			Authentication result = authenticationManager.authenticate(request);
+
+			SecurityContextHolder.getContext().setAuthentication(result);
+
+		} catch (AuthenticationException e) {
+
+			e.printStackTrace();
+
+		}
+
+		return "admin";
+
+	}
+
+}
