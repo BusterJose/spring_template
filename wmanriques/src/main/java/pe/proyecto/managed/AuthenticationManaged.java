c27d8ac618e58e7d6d633c54dc1d8458eae4ccc0
williams manrique <wmanriques@gmail.com>
2012-11-02 13:08:03 -0500

    remember with deprecated

--- wmanriques/src/main/java/pe/proyecto/managed/AuthenticationManaged.java
+++ wmanriques/src/main/java/pe/proyecto/managed/AuthenticationManaged.java
@@ -1,11 +1,20 @@
 package pe.proyecto.managed;
 
+import javax.faces.context.FacesContext;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.RememberMeAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -13,9 +22,60 @@ public class AuthenticationManaged {
 
 	private String user = null;
 	private String password = null;
+	private String rememberMe = null;
 
 	@Autowired
 	private AuthenticationManager authenticationManager = null;
+
+	@Autowired
+	private RememberMeServices rememberMeServices = null;
+
+	@Autowired
+	private UserDetailsService userDetailsService = null;
+
+	public String login() {
+		try {
+			Authentication result = null;
+			if ("TRUE".equalsIgnoreCase(this.rememberMe)) {
+				UserDetails userDetails = userDetailsService
+						.loadUserByUsername(getUser());
+				RememberMeAuthenticationToken rememberMeAuthenticationToken = new RememberMeAuthenticationToken(
+						"jsfspring-sec", userDetails,
+						userDetails.getAuthorities());
+				HttpServletRequest httpServletRequest = (HttpServletRequest) FacesContext
+						.getCurrentInstance().getExternalContext().getRequest();
+				HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext
+						.getCurrentInstance().getExternalContext()
+						.getResponse();
+				rememberMeServices.loginSuccess(httpServletRequest,
+						httpServletResponse, rememberMeAuthenticationToken);
+				result = rememberMeAuthenticationToken;
+			} else {
+				Authentication request = new UsernamePasswordAuthenticationToken(
+						this.getUser(), this.getPassword());
+				result = authenticationManager.authenticate(request);
+			}
+			SecurityContextHolder.getContext().setAuthentication(result);
+		} catch (AuthenticationException e) {
+			e.printStackTrace();
+		}		
+		return "admin";
+	}
+
+	public String logout() {
+		SecurityContextHolder.clearContext();
+		HttpServletRequest httpServletRequest = (HttpServletRequest) FacesContext
+				.getCurrentInstance().getExternalContext().getRequest();
+		HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext
+				.getCurrentInstance().getExternalContext().getResponse();
+		Cookie cookie = new Cookie("SPRING_SECURITY_REMEMBER_ME_COOKIE", null);
+		cookie.setMaxAge(0);
+		cookie.setPath(httpServletRequest.getContextPath().length() > 0 ? httpServletRequest
+				.getContextPath() : "/");
+		httpServletResponse.addCookie(cookie);
+
+		return "loggedout";
+	}
 
 	public String getUser() {
 		return user;
@@ -42,21 +102,28 @@ public class AuthenticationManaged {
 		this.authenticationManager = authenticationManager;
 	}
 
-	public String login() {
-		try {
-			Authentication request = new UsernamePasswordAuthenticationToken(
-					this.getUser(), this.getPassword());
-			Authentication result = authenticationManager.authenticate(request);
-			SecurityContextHolder.getContext().setAuthentication(result);
-		} catch (AuthenticationException e) {
-			e.printStackTrace();
-		}
-		return "admin";
+	public RememberMeServices getRememberMeServices() {
+		return rememberMeServices;
 	}
 
-	public String logout() {
-		SecurityContextHolder.clearContext();
-		return "loggedout";
+	public void setRememberMeServices(RememberMeServices rememberMeServices) {
+		this.rememberMeServices = rememberMeServices;
+	}
+
+	public UserDetailsService getUserDetailsService() {
+		return userDetailsService;
+	}
+
+	public void setUserDetailsService(UserDetailsService userDetailsService) {
+		this.userDetailsService = userDetailsService;
+	}
+
+	public String getRememberMe() {
+		return rememberMe;
+	}
+
+	public void setRememberMe(String rememberMe) {
+		this.rememberMe = rememberMe;
 	}
 
 }
